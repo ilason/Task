3bb0beb9c7172c8c99755b1352b82442fb8d7a48
Aaron Eliason <aaroneliason@hotmail.com>
2016-04-14 16:31:15 -0600

    Core Data set up. and Task Controller set up

---
+++ Task/TaskController.swift
@@ -0,0 +1,63 @@
+//
+//  TaskController.swift
+//  Task
+//
+//  Created by Aaron Eliason on 4/14/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class TaskController {
+    private let taskKey = "tasks"
+    
+    static let sharedController = TaskController()
+    
+    
+    // MARK: I need help having this explained to me
+    
+    var tasks: [Task] {
+        let request = NSFetchRequest(entityName: "Task")
+        
+        do {
+            return try Stack.sharedStack.managedObjectContext.executeFetchRequest(request) as! [Task]
+        } catch {
+            return []
+        }
+    }
+    
+    var completedTasks: [Task] {
+        return tasks.filter({$0.isComplete.boolValue})
+    }
+    
+    var incompleteTasks: [Task] {
+        return tasks.filter({!$0.isComplete.boolValue})
+    }
+    
+    func addTask(task: Task) {
+        saveToPersistentStorage()
+    }
+    
+    func removeTask(task: Task) {
+        task.managedObjectContext?.deleteObject(task)
+        saveToPersistentStorage()
+    }
+    
+    func saveToPersistentStorage() {
+        do {
+            try Stack.sharedStack.managedObjectContext.save()
+        } catch {
+            print("Error")
+        }
+    }
+    
+    func filePath(key: String ) -> String {
+        let directorySearchResults = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.AllDomainsMask, true)
+        let documentsPath: AnyObject = directorySearchResults[0]
+        let entriesPath = documentsPath.stringByAppendingString("/\(key).plist")
+        
+        return entriesPath
+    }
+    
+}
